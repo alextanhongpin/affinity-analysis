c9bbefb5b68bbef7b90f0ecb8f89078e3b7c348c
alextanhongpin <alextan@seekasia.com>
2018-01-04 12:08:38 +0800

    chore: add sample data

--- javascript/fp-growth.js
+++ javascript/fp-growth.js
@@ -1,15 +1,15 @@
-const transactions = [
-  ['a', 'b'],
-  ['b', 'c', 'd'],
-  ['a', 'c', 'd', 'e'],
-  ['a', 'd', 'e'],
-  ['a', 'b', 'c'],
-  ['a', 'b', 'c', 'd'],
-  ['a'],
-  ['a', 'b', 'c'],
-  ['a', 'b', 'd'],
-  ['b', 'c', 'e']
-]
+// const transactions = [
+//   ['a', 'b'],
+//   ['b', 'c', 'd'],
+//   ['a', 'c', 'd', 'e'],
+//   ['a', 'd', 'e'],
+//   ['a', 'b', 'c'],
+//   ['a', 'b', 'c', 'd'],
+//   ['a'],
+//   ['a', 'b', 'c'],
+//   ['a', 'b', 'd'],
+//   ['b', 'c', 'e']
+// ]
 
 // FrequentPatternGrowth
 class FPGrowth {
@@ -69,23 +69,12 @@ class FPGrowth {
   }
 }
 
-const words = [
-  ['hello', 'world'],
-  ['this', 'is', 'amazing'],
-  ['this', 'is', 'real'],
-  ['why', 'is', 'it', 'good'],
-  ['vue', 'js'],
-  ['vue', 'is', 'good'],,
-  ['vue', 'js'],
-  ['vue', 'is', 'bad'],
-  ['is', 'this', 'good']
-]
-
+const words = [['golang', 'string'], ['json', 'golang'], ['golang', 'for'], ['golang', 'file'], ['golang', 'http'], ['map', 'golang'], ['golang', 'array'], ['golang', 'struct'], ['golang', 'example'], ['slice', 'golang'], ['time', 'golang'], ['golang', 'type'], ['golang', 'api'], ['golang', 'server'], ['golang', 'interface'], ['golang', 'github'], ['golang', 'test'], ['golang', 'install'], ['golang', 'package'], ['python'], ['golang', 'error'], ['docker', 'golang'], ['golang', 'channel'], ['golang', 'tutorial'], ['golang', 'set'], ['RISING'], ['golang', '1.9'], ['golang', '1.8.1'], ['golang', 'dep'], ['golang', 'zap'], ['golang', 'chi'], ['golang', '1.8'], ['golang', 'graphql'], ['kubernetes'], ['cobra', 'golang'], ['vscode'], ['jetbrains', 'golang'], ['vscode', 'golang'], ['udemy'], ['golang', 'custom', 'error'], ['golang', 'grpc'], ['aws', 'lambda', 'golang'], ['golang', 'float', 'to', 'string'], ['golang', 'global', 'variables'], ['golang', 'context'], ['golang', 'map', 'reduce'], ['golang', 'microservices'], ['golang', 'firebase'], ['glide', 'golang'], ['golang', 'select', 'channel'], ['golang', 'html', 'parser']]
 console.log(words)
-const fp = new FPGrowth(transactions)
+const fp = new FPGrowth(words)
 fp.generateHeaderTable()
 fp.mapFrequentPattern()
 console.log(fp)
 console.log(JSON.stringify(fp.table, null, 2))
-console.log('found al:', fp.find(['this', 'is'], null, 2))
-console.log('found al:', fp.find(['a', 'b'], null, 2))
+
+console.log('found golang:', fp.find(['golang'], null, 2))
