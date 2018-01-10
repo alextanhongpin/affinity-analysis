1d5f27355ba4e5ecda419c427d39117199bb69ca
alextanhongpin <alextan@seekasia.com>
2018-01-10 19:34:30 +0800

    chore: update logic

--- javascript/fp-growth.v3.js
+++ javascript/fp-growth.v3.js
@@ -1,98 +1,112 @@
-function constructSupport (transactionDb) {
-  const frequentItems = transactionDb.reduce((tree, transaction) => {
-    return transaction.reduce((tree, item) => {
-      if (!tree[item]) {
-        tree[item] = 0
-      }
-      tree[item] += 1
-      return tree
-    }, tree)
-  }, {})
-  return frequentItems
-}
-
-function getSetOfFrequentItems (transactionDb) {
-  // The set of frequent items, F
-  const frequentItems = transactionDb.reduce((f, item) => {
-    f[item.sort().join('')] = true
-    return f
-  }, {})
-  return Object.keys(frequentItems).map((item) => {
-    return item.split('')
-  })
-}
-
-// function constructListOfFrequentItems (frequentItems, minimumSupport = 1) {
-//   return Object.entries(frequentItems).filter(([name, score]) => {
-//     return score >= minimumSupport
-//   }).reduce((frequentItems, [name, score]) => {
-//     frequentItems[name] = score
-//     return frequentItems
-//   }, {})
-// }
-
-function sortAsListOfFrequentItems (support, frequentItems) {
-  // Sort F in support-descending order as FList, the list of frequent items
-  return frequentItems.map(transaction => {
-    return transaction.sort((a, b) => {
-      if (support[a] === support[b]) {
-        return b - a
-      }
-      return support[b] - support[a]
-    })
-  })
-}
 
 class Node {
   constructor (itemName, parentLink) {
-    // Each node of the item-prefix subtree consists of three fields
-    this.itemName = itemName
     this.parentLink = parentLink
+    this.itemName = itemName
     this.count = 1
-    this.nodeLink = null
     this.children = {}
+    this.nodeLink = null
   }
   increment () {
     this.count += 1
   }
 }
 
-function insertTree (p, P, T) {
-  const N = T.children[p]
-  if (N) {
-    N.increment()
-  } else {
-    const newN = new Node(p.itemName)
-    newN.parentNode = T
-  }
-}
-
-function constructFPTree (transactionDb, minimumSupport = 1) {
-  // Collect F, the set of frequent items
-  const setOfFrequentItems = getSetOfFrequentItems(transactionDb)
-  console.log('setOfFrequentItems:', setOfFrequentItems)
-
-  const listOfFrequentItems = sortAsListOfFrequentItems(constructSupport(transactionDb), transactionDb)
-  console.log('listOfFrequentItems:', listOfFrequentItems)
-
-  // Create the root of an FP-tree, T and label it as null
-  const T = new Node(null)
-
-  return T
-}
-
 function main () {
   const transactionDb = [
-    ['a', 'b', 'd', 'e'],
-    ['b', 'c', 'e'],
-    ['a', 'b', 'd', 'e'],
-    ['a', 'b', 'c', 'e'],
-    ['a', 'b', 'c', 'd', 'e'],
-    ['b', 'c', 'd']
+    ['M', 'O', 'N', 'K', 'E', 'Y'],
+    ['D', 'O', 'N', 'K', 'E', 'Y'],
+    ['M', 'A', 'K', 'E'],
+    ['M', 'U', 'C', 'K', 'Y'],
+    ['C', 'O', 'O', 'K', 'I', 'E']
   ]
+  // First iteration through the transaction db
+  const listOfFrequentItems = collectFrequentItems(transactionDb)
+  console.log('listOfFrequentItems:', listOfFrequentItems)
 
-  const fpTree = constructFPTree(transactionDb, 3)
-  console.log('fpTree:', fpTree)
+  const headerTable = constructHeaderTable(listOfFrequentItems)
+  console.log('headerTable:', headerTable)
+
+  // Second iteration through the transaction db
+  const rootNode = new Node(null, null)
+  console.log('rootNode', rootNode)
+  transactionDb.forEach((transaction) => {
+    let currNode = rootNode
+
+    // Remove duplicates, sort it in order
+    const validTransaction = [...new Set(transaction)].filter((itemName) => {
+      return headerTable[itemName]
+    }).sort((a, b) => {
+      return headerTable[b][0] - headerTable[a][0]
+    })
+
+    console.log('validTransaction:', validTransaction)
+
+    console.log('currNode:', currNode)
+
+    validTransaction.forEach((itemName) => {
+      if (currNode.itemName === itemName) {
+        currNode.increment()
+      } else {
+        // If the item already exist...
+        if (currNode.children[itemName]) {
+          currNode.children[itemName].increment()
+        } else {
+          currNode.children[itemName] = new Node(itemName, currNode.itemName)
+        }
+
+        if (headerTable[itemName][1]) {
+          // Already exist, change to something else
+          let item = headerTable[itemName][1]
+          while (item.nodeLink) {
+            item = item.nodeLink
+          }
+          item.nodeLink = currNode.children[itemName]
+          console.log('item:', item)
+        } else {
+          // Link to header table pointer
+          // Create one if it doesn't exist
+          headerTable[itemName][1] = currNode.children[itemName]
+        }
+        currNode = currNode.children[itemName]
+      }
+    })
+  })
+  console.log('rootNode', JSON.stringify(rootNode, null, 2))
+  console.log('headerTable', JSON.stringify(headerTable, null, 2))
 }
 
 main()
+
+function collectFrequentItems (transactionDb, minimumSupport = 3) {
+  const support = transactionDb.reduce((support, transaction) => {
+    // Remove duplicate items from the transaction
+    return [...new Set(transaction)].reduce((supp, item) => {
+      if (!supp[item]) {
+        supp[item] = 0
+      }
+      supp[item] += 1
+      return supp
+    }, support)
+  }, {})
+
+  const listOfFrequentItems = Object.entries(support)
+  .filter(([itemName, score]) => {
+    return score >= minimumSupport
+  }).reduce((header, [itemName, score]) => {
+    header[itemName] = score
+    return header
+  }, {})
+
+  return listOfFrequentItems
+}
+
+function constructHeaderTable (listOfFrequentItems) {
+  const headerTable = Object.entries(listOfFrequentItems)
+  .reduce((headerTable, [itemName, score]) => {
+    // NodeLink
+    headerTable[itemName] = [score, null]
+    return headerTable
+  }, {})
+  return headerTable
+}
