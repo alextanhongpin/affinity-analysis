1ece4d74825eff4c6a5340213f2754a7b28602f2
alextanhongpin <alextan@seekasia.com>
2018-01-11 08:25:28 +0800

    chore: update logic

--- javascript/fp-growth.v3.js
+++ javascript/fp-growth.v3.js
@@ -20,6 +20,14 @@ function main () {
     ['M', 'U', 'C', 'K', 'Y'],
     ['C', 'O', 'O', 'K', 'I', 'E']
   ]
+  // const transactionDb = [
+  //   ['A', 'B', 'D', 'E'],
+  //   ['B', 'C', 'E'],
+  //   ['A', 'B', 'D', 'E'],
+  //   ['A', 'B', 'C', 'E'],
+  //   ['A', 'B', 'C', 'D', 'E'],
+  //   ['B', 'C', 'D']
+  // ]
   // First iteration through the transaction db
   const listOfFrequentItems = collectFrequentItems(transactionDb)
   console.log('listOfFrequentItems:', listOfFrequentItems)
@@ -40,10 +48,6 @@ function main () {
       return headerTable[b][0] - headerTable[a][0]
     })
 
-    console.log('validTransaction:', validTransaction)
-
-    console.log('currNode:', currNode)
-
     validTransaction.forEach((itemName) => {
       if (currNode.itemName === itemName) {
         currNode.increment()
@@ -52,31 +56,33 @@ function main () {
         if (currNode.children[itemName]) {
           currNode.children[itemName].increment()
         } else {
-          currNode.children[itemName] = new Node(itemName, currNode.itemName)
+          currNode.children[itemName] = new Node(itemName, currNode)
         }
 
         if (headerTable[itemName][1]) {
           // Already exist, change to something else
           let item = headerTable[itemName][1]
-          console.log('check:', item.nodeLink)
           while (item.nodeLink) {
             item = item.nodeLink
           }
-          console.log('check_end:', item)
-          item.nodeLink = new Node(itemName, currNode.itemName)// currNode.children[itemName]
-          console.log('item:', item)
+          item.nodeLink = new Node(itemName, currNode)
         } else {
           // Link to header table pointer
           // Create one if it doesn't exist
           headerTable[itemName][1] = currNode.children[itemName]
         }
+
         currNode = currNode.children[itemName]
       }
     })
   })
 
-  console.log('rootNode', JSON.stringify(rootNode, null, 2))
-  console.log('headerTable', JSON.stringify(headerTable, null, 2))
+  const conditionalPatternBases = getConditionalPatternBase(headerTable)
+  console.log('conditionalPatternBases:', conditionalPatternBases)
+
+  const conditionalFPTree = getConditionalFPTree(conditionalPatternBases)
+
+  console.log(JSON.stringify(conditionalFPTree, null, 2))
 }
 
 main()
@@ -112,4 +118,86 @@ function constructHeaderTable (listOfFrequentItems) {
     return headerTable
   }, {})
   return headerTable
+}
+
+function traverseParent (link) {
+  let node = link
+  const conditionalPatternBase = []
+  // let patterns = []
+  do {
+    // Check if the support is still valid
+    // if (node.count >= 3) {
+    //   // Merge
+    //   const out = patterns.concat(node.itemName)
+    //   conditionalPatternBase.push(out)
+    // }
+    conditionalPatternBase.push(node.itemName)
+    // patterns.push(node.itemName)
+    node = node.parentLink
+  } while (node && node.parentLink)
+
+  return conditionalPatternBase.filter(item => item !== null).reverse()
+}
+
+function traverseNodeLink (headerTable, itemName) {
+  let link = headerTable[itemName][1]
+  const conditionalPatternBases = []
+  do {
+    const conditionalPatternBase = traverseParent(link.parentLink)
+    conditionalPatternBases.push(conditionalPatternBase)
+    link = link.nodeLink
+  } while (link && (link.nodeLink || link.parentLink))
+
+  const validPatternBases = conditionalPatternBases.filter(item => item.length)// Take only non-empty arrays
+  .map((items) => {
+    // const flattenedItems = items.map((item) => {
+    //   if (Array.isArray(item)) {
+    //     return item.join(',')
+    //   }
+    //   return item
+    // })
+    // return i.concat(flattenedItems)
+    // if (Array.isArray(item)) {
+    //   return item.join
+    // }
+    return items.join(',')
+  }) // Join them
+
+  const output = [...new Set(validPatternBases)].map((item) => item.split(',')) // Remove duplicates
+  return output
+}
+
+function getConditionalPatternBase (headerTable) {
+  const items = Object.keys(headerTable)
+  const conditionalPatternBases = items.reduce((tree, item) => {
+    const conditionalPatternBase = traverseNodeLink(headerTable, item)
+    if (conditionalPatternBase.length) {
+      tree[item] = conditionalPatternBase
+    }
+
+    return tree
+  }, {})
+  return conditionalPatternBases
+}
+
+function getConditionalFPTree (conditionalPatternBases) {
+  const items = Object.keys(conditionalPatternBases)
+  return items.map(item => {
+    const commonItems = intersect(conditionalPatternBases[item])
+    return [item, commonItems]
+  })
+}
+
+function intersect (arr) {
+  if (arr.length <= 1) {
+    return arr
+  }
+  // Sort the arrays by items count - we only want to find the
+  // intersection of the least items in the array
+  const sortedArr = arr.sort((a, b) => a.length - b.length)
+  const head = sortedArr[0]
+  const tail = sortedArr.slice(1)
+  return head.filter((item) => {
+    return tail.every((i) => new Set(i).has(item))
+  })
 }
