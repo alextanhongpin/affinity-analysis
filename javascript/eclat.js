9ca8e723053a55c90f5d2e263beedd3dbfa7dfe8
alextanhongpin <alextan@seekasia.com>
2018-01-18 21:45:15 +0800

    refactor: eclat javascript

--- javascript/eclat.js
+++ javascript/eclat.js
@@ -1,65 +1,74 @@
-// ECLAT - Equivalance Class Clustering and Bottom up Lattice Traversal
-
 const { dataset7: transactions } = require('./core/data')
 
-function transpose (transactions) {
-  return transactions.reduce((table, transaction, transactionId) => {
-    return transaction.reduce((_table, item) => {
-      if (_table[item] === undefined) {
-        _table[item] = []
-      }
-      _table[item].push(transactionId + 1)
-      return _table
-    }, table)
-  }, {})
-}
-
-function intersect (a, b) {
-  const setA = new Set(a)
-  return b.filter((item) => {
-    return setA.has(item)
-  })
-}
-function union (a, b) {
-  return [...new Set([...a, ...b])]
-}
-
-function combine (tids, minSup, visited = {}, out = []) {
-  let results = []
-  tids.forEach((t1, i) => {
-    tids.forEach((t2, j) => {
-      if (i !== j) {
-        const u = intersect(t1[1], t2[1])
-        const comb = union(t1[0], t2[0])
-        const key = comb.sort().join(',')
-        if (u.length >= minSup && !visited[key]) {
-          visited[key] = true
-          results.push([comb, u])
-        }
-      }
-    })
-  })
-  if (results.length) {
-    out.push(results)
-    combine(results, minSup, visited, out)
+class Eclat {
+  constructor ({ minsup = 2 }) {
+    this.minsup = minsup
   }
-  return out.reduce((a, b) => a.concat(b), [])
+  run (transactions) {
+    const vert = this.mapToVerticalFormat(transactions)
+    return this.mineFrequentItemsets(vert, 2)
+  }
+  uniqueItems (items) {
+    return [...new Set(items)]
+  }
+  mapToVerticalFormat (transactions) {
+    const vert = transactions.reduce((table, items, tid) =>
+      this.uniqueItems(items).reduce((_table, item) => {
+        _table[item] !== undefined ? _table[item].push(tid + 1) : _table[item] = [tid + 1]
+        return _table
+      }, table)
+    , {})
+
+    return Object.entries(vert).map(([item, tids]) => {
+      return [[item], tids]
+    })
+  }
+  union (arr1, arr2) {
+    return this.uniqueItems([...arr1, ...arr2]).sort()
+  }
+  mineFrequentItemsets (transactions, k = 2) {
+    const out = this.frequentItemsets(transactions, k)
+    if (!out.length) return []
+
+    return out.concat(this.mineFrequentItemsets(out, k + 1))
+  }
+  frequentItemsets (transactions, k = 2) {
+    let output = []
+    let found = {}
+    transactions.forEach(([itemset1, tid1], i) => {
+      transactions.forEach(([itemset2, tid2], j) => {
+        if (i <= j) return
+        const combinations = this.union(itemset1, itemset2)
+        const tids = this.intersection(tid1, tid2)
+        const key = combinations.join(',')
+        if (combinations.length >= k && tids.length >= this.minsup && !found[key]) {
+          found[key] = true
+          output.push([combinations, tids])
+        }
+      })
+    })
+    return output
+  }
+  intersection (arr1, arr2) {
+    const [target, compare] = arr1.length > arr2.length ? [arr1, new Set(arr2)] : [arr2, new Set(arr1)]
+    return target.filter((item) => {
+      return compare.has(item)
+    })
+  }
 }
 
 function main () {
-  const minSup = 2
-  const tids = Object.entries(transpose(transactions)).reduce((a, [name, tids]) => {
-    a.push([[name], tids])
-    return a
-  }, [])
-  console.log(tids)
-
-  const out = combine(tids, minSup)
-
-  out.forEach((item) => {
-    const combination = `{${item[0].join(',')}}`
-    console.log(combination, '=>', item[1])
-  })
+  const eclat = new Eclat({ minsup: 2 })
+  const model = eclat.run(transactions)
+  console.log(model)
+  // [ [ [ 'bread', 'butter' ], [ 1, 4, 8, 9 ] ],
+  // [ [ 'bread', 'jam' ], [ 1, 8 ] ],
+  // [ [ 'butter', 'jam' ], [ 1, 8 ] ],
+  // [ [ 'butter', 'coke' ], [ 2, 4 ] ],
+  // [ [ 'bread', 'milk' ], [ 5, 7, 8, 9 ] ],
+  // [ [ 'butter', 'milk' ], [ 3, 6, 8, 9 ] ],
+  // [ [ 'bread', 'butter', 'jam' ], [ 1, 8 ] ],
+  // [ [ 'bread', 'butter', 'milk' ], [ 8, 9 ] ] ]
 }
 
 main()
--- javascript/fp-growth.js
+++ javascript/fp-growth.js
@@ -1,146 +1,17 @@
-const { dataset6: data } = require('./core/data')
-const Node = require('./core/node')
-const HeaderTable = require('./core/header-table')
-const printTree = require('./core/print')
-
-function sortAndFilterRow (row, headerTable) {
-  return [...new Set(row)] // Remove duplicate
-  .filter(item => {
-    const score = headerTable[item]
-    const isFrequent = score !== null && score !== undefined
-    return isFrequent
-  })
-  .sort((a, b) => {
-    const scoreA = headerTable[a][0]
-    const scoreB = headerTable[b][0]
-
-    if (scoreA === scoreB) {
-      // Sort alphabetically in ascending order
-      return a > b
-    }
-    // Sort by score, from highest to lowest
-    return scoreB - scoreA
-  })
-}
-
-function linkHeaderToNode (headerTable, itemName, nodeLink) {
-  const currNodeLink = headerTable[itemName][1]
-  if (!currNodeLink) {
-    headerTable[itemName][1] = nodeLink
-    return
-  }
-  let node = currNodeLink
-  while (node.nodeLink) {
-    node = node.nodeLink
-  }
-  node.nodeLink = nodeLink
-}
-
-function constructConditionalFPTree (transactionDb, headerTable) {
-  const rootNode = new Node(null, null)
-  transactionDb.forEach(row => {
-    let root = rootNode
-    sortAndFilterRow(row, headerTable).forEach(item => {
-      const child = root.children[item]
-      if (child && child.itemName === item) {
-        child.increment()
-      } else {
-        root.children[item] = new Node(item, root)
-        linkHeaderToNode(headerTable, item, root.children[item])
-      }
-      root = root.children[item]
-    })
-  })
-  return rootNode
-}
-
-function constructConditionalPatternBase (headerTable, items, itemName, prefs = []) {
-  const conditionalPatternBases = traverseNodeLink(headerTable[itemName][1])
-  const prefixPath = prefs && prefs.length ? prefs : [itemName]
-
-  const _headerTable = HeaderTable(conditionalPatternBases, 3)
-  const fpTree = constructConditionalFPTree(conditionalPatternBases, _headerTable)
-  console.log(`FP-Tree for ${prefixPath}:`)
-  printTree(fpTree)
-
-  const frequentList = Object.keys(_headerTable)
-  const frequentListWithScore = Object.entries(_headerTable)
-  console.log(`F-list for ${prefixPath}: ${frequentListWithScore.map((item) => item[0] + ':' + item[1][0])}`)
-  console.log()
-
-  appendFrequentItems(items, prefixPath, frequentList)
-
-  frequentList.forEach((itemNm) => {
-    const newPrefs = [itemName, itemNm].concat(prefs)
-    constructConditionalPatternBase(_headerTable, items, itemNm, newPrefs)
-  })
-}
-
-function fpSet (items) {
-  return [...new Set(items)].sort().join(',')
-}
-
-function appendFrequentItems (items, prefixPath, frequentList) {
-  const union = fpSet(prefixPath.concat(frequentList))
-  const prefix = fpSet(prefixPath)
-  items.add(union)
-  items.add(prefix)
-}
-
-function ascendParentLink (node) {
-  const conditionalPatternBase = []
-  let currNode = node
-  do {
-    currNode = currNode.parentLink
-    if (currNode.itemName) {
-      conditionalPatternBase.push(currNode.itemName)
-    }
-  } while (currNode && currNode.parentLink)
-  return conditionalPatternBase
-}
-
-function traverseNodeLink (node) {
-  const conditionalPatternBases = []
-  let currNode = node
-
-  do {
-    const count = currNode.count
-    const conditionalPatternBase = ascendParentLink(currNode)
-    if (conditionalPatternBase.length) {
-      Array(count).fill(conditionalPatternBase).forEach((patternBase) => {
-        conditionalPatternBases.push(patternBase)
-      })
-    }
-    currNode = currNode.nodeLink
-  } while (currNode && (currNode.nodeLink || currNode.parentLink))
-
-  return conditionalPatternBases
-}
-
-function main () {
-  const minimumSupport = 3
-  const transactionDb = data
-
-  const headerTable = HeaderTable(transactionDb, minimumSupport)
-  const fpTree = constructConditionalFPTree(transactionDb, headerTable)
-
-  printTree(fpTree)
-
-  const items = new Set()
-  const prefixPaths = Object.keys(headerTable)
-  prefixPaths.forEach((prefix) => {
-    constructConditionalPatternBase(headerTable, items, prefix)
-  })
-
-  const itemsArr = [...items]
-  const frequentItems = itemsArr.map((i) => sortAndFilterRow(i.split(','), headerTable))
-  console.log('frequentItems:', frequentItems, frequentItems.length)
-}
 
 // TODO: Make it modular
 // const fpg = new FPGrowth().setMinSupport(0.2).setNumPartitions(10)
 // model = fpg.run(transactions)
 // model.freqItemsets.collect().forEach((...))
 // model.generateAssociationRules(minConfidence).collect().forEach((rule) => rule.consequent, rule.antecedent, rule.confidence)
+const { dataset7: transactions } = require('./core/data')
+const FPGrowth = require('./index')
+
+function main () {
+  const fpg = new FPGrowth({ minSupport: 2 })
+  const model = fpg.run(transactions)
+  fpg.print()
+  console.log('model', model, model.length)
+}
 
 main()
