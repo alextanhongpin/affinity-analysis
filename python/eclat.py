ba7bc583a1fe790fa508b92705e8739b9ec2e60e
alextanhongpin <alextan220990@gmail.com>
2018-08-17 01:45:17 +0800

    chore: reduce the iteration to optimize performance

--- python/eclat.py
+++ python/eclat.py
@@ -12,7 +12,9 @@ def main():
                ['bread', 'butter', 'milk', 'jam'],
                ['bread', 'butter', 'milk']]
 
-    (db, items) = vertical_format(dataset)
+    items = reduce(lambda x, y: set(x) | set(y), dataset)
+
+    db = vertical_format(dataset)
     print('db', db)
     solution = {}
     eclat(set(), items, db, 2, solution)
@@ -20,29 +22,17 @@ def main():
     for s in solution:
         print(s, '=>', solution[s])
 
-
-def vertical_format(db):
-    '''converts the db transactions into unique set of transaction ids for each items'''
+def vertical_format(txs):
     # Holds the vertical format data - the key will be the items, and the value will be a unique set of transaction ids
-    vert = {}
-
-    # Holds a unique set of items
-    items = set()
-
-    # Iterate through each transactions in the database
-    for tid, txs in enumerate(db):
-
-        # Append each item to the set
-        items = items | set(txs)
-
-        # For each item in transactions....
-        for t in txs:
-            # Append the transaction ids into the vertical format dictionary
-            vert[t] = vert.get(t, set()) | set([tid])
-    return (vert, items)
+    vert_txs = {}
+    for tid, tx_i in enumerate(txs):
+        for i in tx_i:
+            vert_txs[i] = vert_txs.get(i, set()) | set([tid])
+    return vert_txs
 
 
 def eclat(prefix, items, db, min_sup=2, solution={}, k = 2):
+    items_copy = items.copy()
     for item in items:
         if item in prefix: continue
         new_prefix = prefix | set([item])
@@ -50,7 +40,9 @@ def eclat(prefix, items, db, min_sup=2, solution={}, k = 2):
         if freq >= min_sup:
             if len(new_prefix) >= k:
                 solution[frozenset(new_prefix)] = tids
-            eclat(new_prefix, items - set([item]), db, min_sup, solution)
+            # Remove items for each iteration, to avoid duplications
+            items_copy.discard(item)
+            eclat(new_prefix, items_copy, db, min_sup, solution)
 
 def frequency(prefix, db):
     # Transaction ids for each item with the prefix
