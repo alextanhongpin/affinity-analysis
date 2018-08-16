51905a3986f28d4cf7f798981abf4524e023af3d
alextanhongpin <alextan220990@gmail.com>
2018-08-16 23:21:57 +0800

    chore: add python eclat

---
+++ python/README.md
@@ -0,0 +1,86 @@
+# Eclat
+
+Eclat algorithm stands for *Equivalence Class Clustering and bottom up Lattice Traversal*.
+
+## Horizontal Data Format
+
+Each row in the table here represents a transaction by a user (e.g. a purchase in a supermarket). Here we represent the items as name (bread, butter), but this is normally represented as SKU (Stock Keeping Unit), which is a number assigned to a product by a retail store to identify the price and product options.
+
+
+| TID | Item Set |
+| --- | ----- |
+| 1 | bread, butter, jam |
+| 2 | butter, coke |
+| 3 | butter, milk |
+| 4 | bread, butter, coke |
+| 5 | bread, milk |
+| 6 | butter, milk |
+| 7 | bread, milk |
+| 8 | bread, butter, milk, jam |
+| 9 | bread, butter, milk |
+
+## Vertical Data Format
+
+While _apriori_ and _FP-Growth_ normally uses the horizontal data format, Eclat uses the vertical data format, which is basically a representing each row by item and the transaction ids where the item exists:
+
+| Item | TID Set |
+| ---- | ------- |
+| bread | 1, 4, 5, 7, 8, 9 |
+| butter | 1, 2, 3, 4, 6, 8, 9 |
+| milk | 3, 5, 6, 7, 8, 9 |
+| coke | 2, 4 |
+| jam | 1, 8 |
+
+This is basically the frequent 1-itemsets. 
+In python, this can be represented as a dictionary of sets:
+
+```python
+vert = {
+	'bread': set([1, 4, 5, 7, 8, 9]),
+	# ...
+}
+```
+
+A set is makes it easier later to find the intersection of the transaction ids.
+
+
+## Frequent 2-itemsets
+
+If our `min_sup = 2`, then the last two rows can be excluded, since they did not meet the minimum support.
+
+| Item Set | TID Set |
+| -------- | ------- |
+| {bread, butter} | 1, 4, 8, 9 |
+| {bread, milk} | 5, 7, 8, 9 |
+| {bread, jam} | 1, 8 |
+| {butter, milk} | 3, 6, 8, 9 |
+| {butter, coke} | 2, 4 |
+| {butter, jam} | 1, 8 |
+| {milk, jam} | 8 |
+| {bread, coke} | 4 |
+
+## Frequent 3-itemsets
+
+| Item Set | TID Set | 
+| -------- | ------- |
+| {bread, butter, milk} | 8, 9 |
+| {bread, butter, jam} | 1, 8 |
+
+## Run
+
+```
+$ python eclat.py
+```
+
+Output:
+
+```
+frozenset({'bread', 'milk'}) => {8, 4, 6, 7}
+frozenset({'bread', 'milk', 'butter'}) => {8, 7}
+frozenset({'bread', 'butter'}) => {0, 8, 3, 7}
+frozenset({'bread', 'butter', 'jam'}) => {0, 7}
+frozenset({'bread', 'jam'}) => {0, 7}
+frozenset({'milk', 'butter'}) => {8, 2, 5, 7}
+frozenset({'coke', 'butter'}) => {1, 3}
+frozenset({'butter', 'jam'}) => {0, 7}
+```
--- python/eclat.py
+++ python/eclat.py
@@ -1,27 +1,62 @@
-def main():
-    threshold = 2
-    dataset = [['bread', 'butter', 'jam'],
-        ['butter', 'coke'],
-        ['butter', 'milk'],
-        ['bread', 'butter', 'coke'],
-        ['bread', 'milk'],
-        ['butter', 'milk'],
-        ['bread', 'milk'],
-        ['bread', 'butter', 'milk', 'jam'],
-        ['bread', 'butter', 'milk']]
-    print(dataset)
-    s = {}
-    items = frozenset()
-    for (tid, txs) in enumerate(dataset):
-        for t in txs:
-            s[t] = s[t] | frozenset([tid]) if s.get(t) else frozenset([tid])
-        items = items | frozenset(txs)
-    print(s, items)
+from functools import reduce
 
-# def eclat(prefix, items):
-#     if len(items) == 0: return
-    
-#     for i in items:
-    
+
+def main():
+    dataset = [['bread', 'butter', 'jam'],
+               ['butter', 'coke'],
+               ['butter', 'milk'],
+               ['bread', 'butter', 'coke'],
+               ['bread', 'milk'],
+               ['butter', 'milk'],
+               ['bread', 'milk'],
+               ['bread', 'butter', 'milk', 'jam'],
+               ['bread', 'butter', 'milk']]
+
+    (db, items) = vertical_format(dataset)
+    print('db', db)
+    solution = {}
+    eclat(set(), items, db, 2, solution)
+    print('solution')
+    for s in solution:
+        print(s, '=>', solution[s])
+
+
+def vertical_format(db):
+    '''converts the db transactions into unique set of transaction ids for each items'''
+    # Holds the vertical format data - the key will be the items, and the value will be a unique set of transaction ids
+    vert = {}
+
+    # Holds a unique set of items
+    items = set()
+
+    # Iterate through each transactions in the database
+    for tid, txs in enumerate(db):
+
+        # Append each item to the set
+        items = items | set(txs)
+
+        # For each item in transactions....
+        for t in txs:
+            # Append the transaction ids into the vertical format dictionary
+            vert[t] = vert.get(t, set()) | set([tid])
+    return (vert, items)
+
+
+def eclat(prefix, items, db, min_sup=2, solution={}, k = 2):
+    for item in items:
+        if item in prefix: continue
+        new_prefix = prefix | set([item])
+        (tids, freq) = frequency(new_prefix, db)
+        if freq >= min_sup:
+            if len(new_prefix) >= k:
+                solution[frozenset(new_prefix)] = tids
+            eclat(new_prefix, items - set([item]), db, min_sup, solution)
+
+def frequency(prefix, db):
+    # Transaction ids for each item with the prefix
+    tids = [db[item] for item in prefix]
+    # Find the intersection of all the transaction ids for each items
+    intersect_tids = reduce(lambda x, y: x & y, tids)
+    return (intersect_tids, len(intersect_tids))
 
 main()
