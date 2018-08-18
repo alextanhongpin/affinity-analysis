1fc294bfd453a2efd4dfd05e876b571c500df595
alextanhongpin <alextan220990@gmail.com>
2018-08-18 19:23:31 +0800

    chore: reduce code loc

--- python/eclat.py
+++ python/eclat.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from random import shuffle
+import time
 
 def main():
     dataset = [['bread', 'butter', 'jam'],
@@ -14,25 +15,29 @@ def main():
 
     # Shuffle to ensure the results are not purely deterministic on order
     # Note that this will change the transaction ids
-    shuffle(dataset)
-    print('dataset', dataset)
+    # shuffle(dataset)
+    # print('dataset', dataset)
 
 
-    dataset = [['mp3-player', 'usb-charger', 'book-dct', 'book-ths'],
-               ['mp3-player', 'usb-charger'],
-               ['usb-charger', 'mp3-player', 'book-dct', 'book-ths'],
-               ['usb-charger'],
-               ['book-dct', 'book-ths']]
+    # dataset = [['mp3-player', 'usb-charger', 'book-dct', 'book-ths'],
+    #            ['mp3-player', 'usb-charger'],
+    #            ['usb-charger', 'mp3-player', 'book-dct', 'book-ths'],
+    #            ['usb-charger'],
+    #            ['book-dct', 'book-ths']]
 
-    items = reduce(lambda x, y: set(x) | set(y), dataset)
-
-    db = vertical_format(dataset)
-    print('db', db)
+    items = reduce(lambda x, y: frozenset(x) | frozenset(y), dataset)
     solution = {}
-    eclat(set(), items, db, 2, solution)
-    print('solution')
+    db = vertical_format(dataset)
+    t0 = time.time()
+    eclat(frozenset(), items, db, 2, solution)
+    t1 = time.time()
 
-    sorted_keys = sorted(solution, key=lambda k: len(solution[k]), reverse=True)
+    print('found {} solutions:'.format(len(solution)), (t1 - t0) * 1000)
+    print_solution(solution)
+
+
+def print_solution(solution):
+    sorted_keys = sorted(solution, key=lambda k: len(k), reverse=True)
     pr = lambda x: ', '.join([str(i) for i in sorted(x)])
     for key in sorted_keys:
         print('{{{}}} => {{{}}}'.format(pr(key),
@@ -41,28 +46,28 @@ def main():
 def vertical_format(db):
     # Holds the vertical format data - the key will be the items, and the value will be a unique set of transaction ids
     result = {}
+    result[frozenset()] = set()
     for id, row in enumerate(db):
+        # Let the empty set hold all value, so that intersection with any empty prefix will return itself
+        result[frozenset()] |= set([id])
         for i in row:
-            result[i] = result.get(i, set()) | set([id])
+            key = frozenset([i])
+            result[key] = result.get(key, set()) | set([id])
     return result
 
 
 def eclat(prefix, items, db, min_sup=2, solution={}):
-    items_copy = items.copy()
+    items_copy = set(items)
     for item in items:
-        new_prefix = prefix | set([item])
-        (tids, freq) = frequency(new_prefix, db)
-        if freq >= min_sup:
-            solution[frozenset(new_prefix)] = tids
-            # Remove items for each iteration, to avoid duplications
-            items_copy.discard(item)
-            eclat(new_prefix, items_copy, db, min_sup, solution)
+        items_copy.discard(item)
 
-def frequency(prefix, db):
-    # Transaction ids for each item with the prefix
-    tids = [db[item] for item in prefix]
-    # Find the intersection of all the transaction ids for each items
-    intersect_tids = reduce(lambda x, y: x & y, tids)
-    return (intersect_tids, len(intersect_tids))
+        item_set = frozenset([item])
+        new_prefix = prefix | item_set
+        tids = db[prefix] & db[item_set]
+
+        if len(tids) >= min_sup:
+            db[new_prefix] = tids
+            solution[new_prefix] = tids
+            eclat(new_prefix, items_copy, db, min_sup, solution)
 
 main()
